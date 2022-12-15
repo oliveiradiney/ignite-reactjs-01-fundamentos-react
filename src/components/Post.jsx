74773e70f8757781dc5965d4ccb3aa26902ff364
Diney Oliveira <diney.oliveira>
2022-12-15 16:00:58 -0300

    Estado(useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,8 +3,16 @@ import { Comment } from './Comment'
 import styles from './Post.module.css'
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
+
+
 
 export function Post({author, publishedAt, content}){
+
+    const [comments, setComments] = useState([
+        1, 
+        2
+    ])
     //Data
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
         locale: ptBR
@@ -14,6 +22,15 @@ export function Post({author, publishedAt, content}){
         locale: ptBR,
         addSuffix: true
     })
+
+    function handleCreateNewComment(){
+        event.preventDefault()
+
+        //imutabilidade
+        setComments([...comments, comments.length + 1])
+
+        console.log(comments)
+    }
 
     return(
         <article className={styles.post}>
@@ -46,7 +63,7 @@ export function Post({author, publishedAt, content}){
                 </p> */}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment}  className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
@@ -59,7 +76,9 @@ export function Post({author, publishedAt, content}){
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
 
 
